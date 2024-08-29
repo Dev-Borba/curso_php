43701952deb8360a664aa7e07fd02d65a1079dba
Gabriel Borba <gabrielborba360@gmail.com>
2024-08-29 09:32:50 -0300

    update

---
+++ 5_op_e_expressoes/30_exercicio_20/index.php
@@ -0,0 +1,11 @@
+<?php
+
+  # Atribua dois números a variáveis distintas;
+  # Faça uma comparação de menor ou igual com o operador ternário;
+  # Imprima resultados para ambas as possibilidades;
+
+  $a = 10;
+  $b = 20;
+
+  echo $a <= $b ? "A é menor ou igual a B <br>" : "B é menor que A <br>";
+  echo $a >= $b ? "A é menor ou igual a B <br>" : "B é menor que A <br>";
