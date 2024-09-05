2e95a8ab096930b950112d2ba63e220ee91daea0
Gabriel Borba <gabrielborba360@gmail.com>
2024-09-05 10:43:59 -0300

    update

---
+++ 6_estruturas_de_controle/02_exercicio_21/index.php
@@ -0,0 +1,27 @@
+<?php
+
+  # Faça as seguintes verificações em estruturas if:
+  # 5 é maior que 2?
+  # Matheus é difrente de Pedro;
+  # 12 é menor ou igual a 11;
+  # Você deve inserir os valores em variáveis.
+
+  if(5 > 2) {
+    echo "5 é maior que 2 <br>";
+  }
+
+  if("Matheus" != "Pedro") {
+    echo "Metheus é diferente de Pedro <br>";
+  }
+
+  if(12 <= 11) {
+    echo "12 é menor ou igual a 11 <br>";
+  }
+
+  $a = 10;
+  $b = 5;
+  $c = "10 é igual ou maior que 20 <br>";
+
+  if($a >= $b) {
+    echo $c;
+  }
---
+++ 6_estruturas_de_controle/03_exercicio_22/index.php
@@ -0,0 +1,8 @@
+<?php
+
+  # Crie uma variável que recebem idades;
+  # Cheque se as idades são maiores ou iguais a 18;
+  # Se sim, imprima uma mensagem que a pessoa é maior de idade;
+
+
+  
